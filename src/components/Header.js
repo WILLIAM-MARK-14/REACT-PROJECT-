55c43e0b05a9eac3adf5f9ec8361e36b09c39fda
WILLIAM-MARK-14 <williamsam9353@gmail.com>
2026-04-27 16:38:40 +0530

    Added  images folder,Created Header Component

---
+++ src/components/Header.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import{Nav,Navbar,NavDropdown,Container,Image}from 'react-bootstrap';
+
+const Header = () => {
+
+     
+    return(
+        <header>
+         <Navbar className='navbar-style' expand="lg">
+          <Container>
+             <Navbar.Brand href="#home" className='navbar-brand'> 
+             <Image className='img-style' src='335850 6.jpg' alt='img'/>
+             <h6 className='logo-text'>Global Virtual Academy for Peace & Education</h6>
+             </Navbar.Brand>
+
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+
+          <Nav className="ml-auto">
+              <Nav.Link href="#">Home</Nav.Link>
+              <Nav.Link href="#link">About Us</Nav.Link>
+              
+
+          <NavDropdown title="Academic" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#">Admissions</NavDropdown.Item>
+             <NavDropdown.Item href="#">Circulars & Notification</NavDropdown.Item>
+             <NavDropdown.Item href="#">Cirtificate Issue</NavDropdown.Item>
+          </NavDropdown>
+
+          <NavDropdown title="Examination" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#">Examination Quidelines</NavDropdown.Item>
+             <NavDropdown.Item href="#">Examination Applications</NavDropdown.Item>
+             <NavDropdown.Item href="#">Results</NavDropdown.Item>
+          </NavDropdown>
+
+          <NavDropdown title="VTU Departments" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#">Civil Engg</NavDropdown.Item>
+             <NavDropdown.Item href="#">Computer Science Engg</NavDropdown.Item>
+             <NavDropdown.Item href="#">Electronice & communication Engg</NavDropdown.Item>
+          </NavDropdown>
+
+          <NavDropdown title="Staff Logins" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#">Login as Admin</NavDropdown.Item>
+          </NavDropdown>
+
+          </Nav>
+
+        </Navbar.Collapse>
+
+          </Container>
+
+         </Navbar>
+        
+        </header>
+    )
+}
+
+export default Header
